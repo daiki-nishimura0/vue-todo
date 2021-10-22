165c847d2c8d76095a8723a79259d1fbb5dfd002
daiki-nishimura0 <89887093+daiki-nishimura0@users.noreply.github.com>
2021-10-22 17:17:47 +0900

    入力フォームの追加 (#2)

---
+++ src/components/Input.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container style="max-width: 500px">
+        <v-text-field
+            v-model="newTask"
+            label="タスクを入力してね"
+            solo
+            @keydown.enter="create"
+        >
+        </v-text-field>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+
+  data: ()=>({
+      
+  })
+}
+</script>
